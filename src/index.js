bb0a90ece4e4b02819d7bc82d5ebaeb38a375039
Daniil Burdakov <icqkill@gmail.com>
2018-11-02 21:36:06 +0000

    Configure eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// @flow
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import nullthrows from "nullthrows";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, nullthrows(document.getElementById("root")));
